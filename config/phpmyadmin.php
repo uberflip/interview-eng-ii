8911c9006df6866a6dc694e50e545bb2b6be7e27
James Booth <james.booth@uberflip.com>
2021-11-23 10:01:00 -0600

    Organized configs

---
+++ config/phpmyadmin.php
@@ -0,0 +1,11 @@
+<?php
+$cfg['Servers'] = [
+    1 => [
+        'auth_type' => 'config',
+        'host' => 'db',
+        'port' => 3306,
+        'user' => 'uberflip',
+        'password' => 'pass123',
+        'hide_db' => 'information_schema'
+    ]
+];
