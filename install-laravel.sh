cddcb1300bbdd658e46dc0cefbd84aedbac4245c
James Booth <james.booth@uberflip.com>
2021-12-14 13:21:01 -0600

    ENG-1390: Updated TailwindCSS install

--- install-laravel.sh
+++ install-laravel.sh
@@ -28,6 +28,11 @@ echo "@tailwind base;" >> resources/css/app.css
 echo "@tailwind components;" >> resources/css/app.css
 echo "@tailwind utilities;" >> resources/css/app.css
 sed -i 's/^        \/\//        require(\d39tailwindcss\d39),/' webpack.mix.js
+sed -i 's/content: []/content: [\
+    "./resources/views/*.blade.php",\
+    "./resources/js/*.js",\
+    "./resources/js/*.ts",\
+  ]' tailwind.config.js
 
 # Copy welcome HTML to default Laravel view, customize
 cp ../web/index.html resources/views/welcome.blade.php
